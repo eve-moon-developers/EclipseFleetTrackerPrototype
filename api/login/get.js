922a4c380f38229c6051bfff09fb7250b890365b
W. Craig Jones <Sciencegeek123@hotmail.com>
2017-02-13 20:47:20 -0600

    Now show who is trying to log in.

--- api/login/get.js
+++ api/login/get.js
@@ -1,5 +1,5 @@
 module.exports.handler = function(req, res, next) {
-    console.log("Request recieved.");
+    console.log("Request recieved: " + req.params.username);
     var auth = require('server/auth.js');
     auth.login(req.params).then(function(data) {
         res.send(data);
